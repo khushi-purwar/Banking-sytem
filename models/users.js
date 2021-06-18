9ba5a8e6ec4a12c520ec95d7460486c5b5daa18b
khushi-purwar <52875298+khushi-purwar@users.noreply.github.com>
2021-06-18 18:59:56 +0530

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,1 +1,32 @@
+const mongoose = require('mongoose');
+const Transaction = require('./transaction');
 
+const userSchema  = new mongoose.Schema(
+    {
+    sno: {
+            type : Number,
+            required: true,
+    },   
+    name : {
+        type: String,
+        required :true,
+    },
+    email : {
+        type: String,
+        required :true,
+    },
+    actno: {
+        type : Number,
+        required: true,
+    },
+    bal : {
+        type: Number,
+        required : true,
+    }
+  
+}
+);
+
+const user = mongoose.model('user', userSchema);
+
+module.exports = user;
